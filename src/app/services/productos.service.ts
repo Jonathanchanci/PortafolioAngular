20f13d1c2695180ddff3208de8fb63ad0fb1680d
Jonathan <jonathan.a.chanci@gmail.com>
2021-05-19 21:30:08 -0500

    se agrega busqueda de items

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,19 +9,50 @@ export class ProductosService {
 
   cargando = true;
   productos : Producto[] = [];
+  productosFiltrado: Producto[] = [];
   constructor(private http: HttpClient) {
     this.cargarProductos();
    }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-11bbe-default-rtdb.firebaseio.com/productos_idx.json')
+
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-11bbe-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe((resp: Producto[]) =>{
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
+    });    
   }
 
   getProducto (id: string){
     return this.http.get(`https://angular-html-11bbe-default-rtdb.firebaseio.com/productos/${id}.json`)
+  }
+
+  buscarProducto(termino: string){
+    //Cargar productos
+    if(this.productos.length == 0){
+      this.cargarProductos().then(()=>{
+        //ejecutar despues de tener los productos
+        //aplicar filtro
+        this.filtrarProducto(termino);
+      });
+    }else{
+      //Aplicar filtro
+      this.filtrarProducto(termino);
+    }
+  }
+
+  private filtrarProducto(termino: string){
+    this.productosFiltrado = [];
+    termino  = termino.toLocaleLowerCase();
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      const categoriaLower = prod.categoria.toLocaleLowerCase();
+      if(categoriaLower.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+        this.productosFiltrado.push(prod);
+      }
+    });
   }
 }
